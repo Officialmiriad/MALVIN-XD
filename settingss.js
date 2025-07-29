57feecec2c86d65f0551bb54cf32794e6e2591cc
Officialmiriad <miriadke991@gmail.com>
2025-07-29 04:12:14 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~tr9GnZTD#dYvwwv3mMNzMHA9J4oldVpTcfRg-X_G8oQ3VmYK2xqc ",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "254112544943", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
